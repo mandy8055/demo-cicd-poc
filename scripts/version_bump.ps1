b3f1093638506a38e69fc47bf16721ecd27207a6
Manuj Sankrit <msankrit8055@gmail.com>
2024-07-06 11:56:48 +0530

    [T20-12] :green_heart: Updated scripts to add input for version_bump

--- scripts/version_bump.ps1
+++ scripts/version_bump.ps1
@@ -1,12 +1,20 @@
 function Increment-Version {
     param(
-        [string]$version
+        [string]$version,
+        [string]$type
     )
     $version_parts = $version.TrimStart('v').Split('.')
     $major = [int]$version_parts[0]
     $minor = [int]$version_parts[1]
     $patch = [int]$version_parts[2]
-    $patch++
+
+    switch ($type) {
+        "major" { $major++; $minor = 0; $patch = 0 }
+        "minor" { $minor++; $patch = 0 }
+        "patch" { $patch++ }
+        default { Write-Error "Invalid version bump type" }
+    }
+
     return "v$major.$minor.$patch"
 }
 
@@ -14,7 +22,7 @@ git fetch --tags
 $current_tag = git describe --tags $(git rev-list --tags --max-count=1)
 if (-not $current_tag) { $current_tag = "v0.0.0" }
 
-$new_tag = Increment-Version -version $current_tag
+$new_tag = Increment-Version -version $current_tag -type $env:VERSION_TYPE
 
 Write-Output "New tag: $new_tag"
 
