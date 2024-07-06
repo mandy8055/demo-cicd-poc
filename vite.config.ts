776a9d56cd269313858fb8e19fa49df8f21ff530
Manuj Sankrit <msankrit8055@gmail.com>
2024-07-06 13:00:48 +0530

    [T20-12] :green_heart: Updated scripts to add input for version_bump

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.BASE_URL || '/',
   test: {
     environment: 'jsdom',
     setupFiles: './src/global/setupTests.ts',
